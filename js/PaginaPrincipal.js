39a90b500a4fc1e85be4ece7aaecc72f94b83334
Gianluca Sanguinetti <giansanguinetti77@gmail.com>
2024-03-07 23:01:57 -0300

    Navbar con Toggle menu finalizado

---
+++ js/PaginaPrincipal.js
@@ -0,0 +1,9 @@
+/*const Admin = true;
+if (Admin) {
+    document.getElementById('AdminUser').style.display = 'inline';
+}
+*/
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("active");
+}
