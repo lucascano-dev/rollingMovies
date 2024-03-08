ca1385fb6023e9d47e7b99f12b7f5fb7e25fd5ce
Gianluca Sanguinetti <giansanguinetti77@gmail.com>
2024-03-08 02:16:30 -0300

    Footer y Navbar con responsive y menu desplegable terminados

--- js/PaginaPrincipal.js
+++ js/PaginaPrincipal.js
@@ -1,9 +1,17 @@
-/*const Admin = true;
-if (Admin) {
-    document.getElementById('AdminUser').style.display = 'inline';
-}
-*/
+
+
 function toggleMenu() {
     var menu = document.getElementById("menu");
     menu.classList.toggle("active");
+}
+
+const AdminLogged = true
+
+window.onload = function() {
+    const AdminUser = document.getElementById("AdminUser");
+    if (AdminLogged) {
+        AdminUser.style.display = "block" ;
+    } else {
+        AdminUser.style.display = "none" ;
+    }
 }
