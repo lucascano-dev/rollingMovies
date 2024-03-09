be9fd2b61b37ef300c0d1f479bb8c4192a321f80
Gianluca Sanguinetti <giansanguinetti77@gmail.com>
2024-03-09 20:43:32 -0300

    Carrousel de la pagina principal terminado

--- js/PaginaPrincipal.js
+++ js/PaginaPrincipal.js
@@ -1,10 +1,12 @@
 
-
+/*Menu Desplegable*/
 function toggleMenu() {
     var menu = document.getElementById("menu");
     menu.classList.toggle("active");
 }
+/*Menu Desplegable*/
 
+/* Mostrar opcion de administrador si el admin es el usuario Logeado*/
 const AdminLogged = true
 
 window.onload = function() {
@@ -15,3 +17,27 @@ window.onload = function() {
         AdminUser.style.display = "none" ;
     }
 }
+/* Mostrar opcion de administrador si el admin es el usuario Logeado*/
+
+/* Carrusel de peliculas*/
+const CarrouselSlide = document.querySelector('.CarrouselSlide');
+const CarrouselImages = document.querySelectorAll('.CarrouselSlide img');
+
+let counter = 0;
+const size = CarrouselImages[0].clientWidth;
+
+CarrouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+function Slide() {
+  if (counter >= CarrouselImages.length - 1) {
+  CarrouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter = 0;
+  } else {
+    counter++;
+    }
+  CarrouselSlide.style.transform= 'translateX(' + (-size * counter) + 'px)';
+}
+
+setInterval(Slide, 3000);
+/* Carrusel de peliculas*/
+
