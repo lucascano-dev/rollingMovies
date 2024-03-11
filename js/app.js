5119d0159399878dcb75a21cb5ba6f9573ca373e
Marcelo de Cristobal <marcelodecristobal87@gmail.com>
2024-03-11 13:12:59 -0300

    Cambio en 404 y detalle pelicula

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
-const openModalBtn = document.getElementById('openModalBtn');
-const modal = document.getElementById('myModal');
-const closeBtn = document.querySelector('.close');
+// Obtén el modal
+let modal = document.getElementById("modalPelicula");
 
-openModalBtn.addEventListener('click', () => {
-    modal.style.display = 'block';
-});
+// Obtén el botón que abre el modal
+let btn = document.getElementById("btnModal");
 
-closeBtn.addEventListener('click', () => {
-    modal.style.display = 'none';
-});
+// Obtén el elemento <span> que cierra el modal
+let span = document.getElementsByClassName("close")[0];
 
+// Cuando el usuario haga clic en el botón, abre el modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Cuando el usuario haga clic en <span> (x), cierra el modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Cuando el usuario haga clic fuera del modal, cierra el modal
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
