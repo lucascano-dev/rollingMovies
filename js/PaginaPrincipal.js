03398d49e2e029bf9630570bf3d2cd22dc6c741c
Gianluca Sanguinetti <giansanguinetti77@gmail.com>
2024-03-10 22:33:24 -0300

    pagina principal con carrusel por categorias armado

--- js/PaginaPrincipal.js
+++ js/PaginaPrincipal.js
@@ -41,4 +41,26 @@ function Slide() {
 setInterval(Slide, 3000);
 /* Carrusel de peliculas*/
 
+/* slider de peliculas por categorias*/
+document.addEventListener("DOMContentLoaded", function() {
+    const comedyRow = document.querySelector(".carousel-row.comedy");
+    const rightArrow = document.querySelector(".right-arrow");
+    const movies = Array.from(comedyRow.querySelectorAll(".movies"));
+    const movieWidth = movies[0].offsetWidth;
+    let currentIndex = 0;
+  
+    rightArrow.addEventListener("click", function() {
+      const moviesLength = movies.length;
+      const remainingMovies = moviesLength - currentIndex;
+      const moviesToScroll = Math.min(5, remainingMovies);
+      
+      currentIndex += moviesToScroll;
+      if (currentIndex >= moviesLength) {
+        currentIndex = 0;
+      }
+  
+      comedyRow.style.transform = `translateX(-${currentIndex * movieWidth}px)`;
+    });
+  });
+/* slider de peliculas por categorias*/
 
