0bf300b99d287ad044c195fee87fe73fc52df18f
Lucas Aaron Cano <lucas_cano@outlook.com>
2024-03-12 01:20:50 -0300

    PanelAdmin carga de datos en tabla de contenido

--- js/panel-admin/p-admin-contenidos.js
+++ js/panel-admin/p-admin-contenidos.js
@@ -1,12 +1,13 @@
 const tablaContenido = document.querySelector('#tablaContenido');
 const titulo = document.querySelector('#encabezado-tablas');
 const dropItem = document.querySelector('.enlace-test');
-const btnPublicado = document.getElementsByClassName('form-check-label');
+const btnPublicado = document.getElementsByClassName('form-check-input');
 const iconoDestacar = document.getElementsByClassName('icono-destacar');
 const btnDestacar = document.getElementsByClassName('btn-destacar');
 const tabla = document.querySelector('#tabla');
 const btnVolver = document.getElementById('btn-volver');
 const btnAdminUsuarios = document.getElementById('btnAdminUsuarios');
+let contenidosMovies = JSON.parse(localStorage.getItem('movies')) || [];
 
 console.log(titulo.textContent);
 
@@ -14,6 +15,53 @@ dropItem.addEventListener('click', (event) => {
   event.preventDefault();
   console.log('Click');
 });
+
+const cargarContenido = function () {
+  tablaContenido.innerHTML = '';
+  contenidosMovies.map(function (movie) {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+                <td class="border">${movie.id}</td>
+                <td class="border">${movie.nombre}</td>
+                <td class="border">${movie.categoria}</td>
+                <td class="border">
+                  <span class="d-inline-block text-truncate" style="width: 200px"
+                    >${movie.descripcion}</span
+                  >
+                </td>
+                <td class="border">
+                  <img
+                    src="${movie.urlImagen}"
+                    alt="${movie.descripcionImagen}"
+                    width="60px"
+                  />
+                </td>
+                <td class="border"><span>${movie.urlVideo}</span></td>
+                <td class="border">
+                  <div class="form-check form-switch">
+                    <input
+                      class="form-check-input"
+                      type="checkbox"
+                      role="switch"
+                      id="flexSwitchCheckChecked1"
+                      
+                    />
+                    <label class="form-check-label" for="flexSwitchCheckChecked1">✅</label>
+                  </div>
+                </td>
+                <td class="border">
+                  <div class="d-flex gap-1 flex-wrap">
+                    <button type="button" class="btn btn-warning"><i class="fa-solid fa-pen-to-square"></i></button>
+                    <button type="button" class="btn btn-secondary btn-destacar">
+                      <i class="fa-solid fa-star icono-destacar contenido-no-destacado"></i>
+                    </button>
+                    <button type="button" class="btn btn-danger"><i class="fa-solid fa-trash-can"></i></button>
+                  </div>
+                </td>`;
+    tablaContenido.appendChild(tr);
+  });
+};
+cargarContenido();
 
 /** Boton Volver */
 btnVolver.addEventListener('click', () => {
@@ -37,7 +85,7 @@ for (let i = 0; i < btnDestacar.length; i++) {
 /** Switch Publicado */
 for (let i = 0; i < btnPublicado.length; i++) {
   btnPublicado[i].addEventListener('click', () => {
-    // if (btnPublicado[i].checked)
+    console.log('Clic en switch de linea:', [i]);
   });
 }
 
--- pages/login-test/js/app.js
+++ pages/login-test/js/app.js
@@ -9,49 +9,49 @@ class Usuario {
   }
 }
 
-const validarRegistro = document.querySelector("#validarRegistro");
+const validarRegistro = document.querySelector('#validarRegistro');
 
-validarRegistro.addEventListener("submit", validarUsuario);
+validarRegistro.addEventListener('submit', validarUsuario);
 
-const usuariosRegistrados = JSON.parse(localStorage.getItem("usuarios")) || [];
+const usuariosRegistrados = JSON.parse(localStorage.getItem('usuarios')) || [];
 console.log(usuariosRegistrados);
 
 function validarUsuario(e) {
   e.preventDefault();
   const id = Date.now();
-  const nombre = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  const confirmPassword = document.getElementById("confirmPassword").value;
+  const nombre = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const confirmPassword = document.getElementById('confirmPassword').value;
   const stateActivation = 0;
-  const userRol = "Admin";
+  const userRol = 'Admin';
 
   const validarEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
   const resultadoValidarEmail = validarEmail.test(email);
 
-  !resultadoValidarEmail ? console.log("Formato de email inválido") : console.log("Formato de email OK");
+  !resultadoValidarEmail ? console.log('Formato de email inválido') : console.log('Formato de email OK');
 
   const comprobandoUsuarioEmail = usuariosRegistrados.find(function (usuario) {
     return usuario.email === email;
   });
 
   comprobandoUsuarioEmail !== undefined
-    ? console.log("Usuario ya registrado")
+    ? console.log('Usuario ya registrado')
     : Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Usuario no registrado",
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Usuario no registrado',
       });
 
   const nuevoUsuario = new Usuario(id, nombre, email, password, stateActivation, userRol);
   usuariosRegistrados.push(nuevoUsuario);
 
-  localStorage.setItem("usuarios", JSON.stringify(usuariosRegistrados));
+  localStorage.setItem('movies', JSON.stringify(usuariosRegistrados));
 
   Swal.fire({
-    position: "center",
-    icon: "success",
-    title: "Usuario registrado correctamente",
+    position: 'center',
+    icon: 'success',
+    title: 'Usuario registrado correctamente',
     showConfirmButton: false,
     timer: 1500,
   });
