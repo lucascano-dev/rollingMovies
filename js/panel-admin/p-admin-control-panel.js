4f90839e44d2f7e2ea03a57f9f7d54333168ffa8
Lucas Aaron Cano <lucas_cano@outlook.com>
2024-03-18 15:07:07 -0300

    correccion de css para pagina de contactos

--- js/panel-admin/p-admin-control-panel.js
+++ js/panel-admin/p-admin-control-panel.js
@@ -2,4 +2,6 @@ const btnPanel = document.getElementById('btn-manipular-panel-izquierdo');
 
 btnPanel.addEventListener('click', () => {
   console.log('TEST');
+  const id = Date.now();
+  console.log(id);
 });
