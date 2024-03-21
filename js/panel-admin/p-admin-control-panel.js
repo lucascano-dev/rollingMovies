2df6e8c9b9bd1993a8054ed69f751e3991ecc5b9
Lucas Aaron Cano <lucas_cano@outlook.com>
2024-03-21 05:13:53 -0300

    fix funcionalidad en boton mostrar/ocultar panel

--- js/panel-admin/p-admin-control-panel.js
+++ js/panel-admin/p-admin-control-panel.js
@@ -3,11 +3,11 @@ const btnPanelOculto_ControlPanel = document.querySelector('#btn-manipular-panel
 const panelExpandido = document.querySelector('.container-panel-lateral-expandido');
 const panelOculto = document.querySelector('.container-panel-lateral-oculto');
 
-if (JSON.parse(localStorage.getItem('estadoPanel')) === 'show') {
-  panelExpandido.style.display = 'block';
-} else {
+if (JSON.parse(localStorage.getItem('estadoPanel')) === 'hide') {
   panelExpandido.style.display = 'none';
-  localStorage.setItem('estadoPanel', JSON.stringify('hide'));
+} else {
+  panelExpandido.style.display = 'block';
+  localStorage.setItem('estadoPanel', JSON.stringify('show'));
 }
 
 btnPanelExpandido_ControlPanel.addEventListener('click', () => {
