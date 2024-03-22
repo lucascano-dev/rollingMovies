f6352d527e53416c7574b259a9d5d18e914c58da
Lucas Aaron Cano <lucas_cano@outlook.com>
2024-03-22 14:24:09 -0300

    add main page

--- js/PaginaPrincipal.js
+++ js/PaginaPrincipal.js
@@ -1,109 +1,19 @@
-<<<<<<< HEAD
-
-function toggleMenu() {
-  var menu = document.getElementById("menu");
-  menu.classList.toggle("active");
-=======
 /*Menu Desplegable*/
 function toggleMenu() {
-	var menu = document.getElementById('menu');
-	menu.classList.toggle('active');
->>>>>>> 476924d952a5cf74e5f297c3aef4278e1336256e
+  var menu = document.getElementById('menu');
+  menu.classList.toggle('active');
 }
 /*Menu Desplegable*/
 
-<<<<<<< HEAD
+/* Mostrar opcion de administrador si el admin es el usuario Logeado*/
 const AdminLogged = true;
 
 window.onload = function () {
-  const AdminUser = document.getElementById("AdminUser");
+  const AdminUser = document.getElementById('AdminUser');
   if (AdminLogged) {
-    AdminUser.style.display = "block";
+    AdminUser.style.display = 'block';
   } else {
-    AdminUser.style.display = "none";
+    AdminUser.style.display = 'none';
   }
 };
-
-//Register button with some validations
-const regUser = document.getElementById("regUser");
-
-regUser.addEventListener("click", () => {
-  let arrayUsers = JSON.parse(localStorage.getItem("usuarios")) || [];
-  let userObject = {
-    nombre: document.getElementById("name").value,
-    correo: document.getElementById("mail").value,
-    pass: document.getElementById("pass").value,
-    confirmPass: document.getElementById("confirmPass").value,
-    isAdmin: false,
-  };
-  let isValid = isValidUser(userObject);
-  if (isValid == true) {
-    arrayUsers.push(userObject);
-    localStorage.setItem("usuarios", JSON.stringify(arrayUsers));
-  }
-  function isValidUser(userObject) {
-    let mailExpression =
-      /^([a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$)/;
-    if (
-      userObject.nombre == "" ||
-      userObject.correo == "" ||
-      userObject.pass == "" ||
-      userObject.confirmPass == ""
-    ) {
-      errorHandling("error", "The fields can not be empty!");
-      return false;
-    } else if (userObject.pass != userObject.confirmPass) {
-      errorHandling("error", "The passwords must match!");
-      return false;
-    } else if (mailExpression.test(userObject.correo) == false) {
-      errorHandling("error", "Please enter a valid email!");
-      return false;
-    } else if (JSON.stringify(arrayUsers).includes(userObject.nombre)) {
-      errorHandling("error", "User already registered!");
-    } else {
-      Swal.fire({
-        title: "User registration successfull!",
-        html: "This window will close in <b></b> seconds.",
-        timer: 5000,
-        timerProgressBar: true,
-        didOpen: () => {
-          Swal.showLoading();
-          const timer = Swal.getPopup().querySelector("b");
-          timerInterval = setInterval(() => {
-            timer.textContent = `${parseInt(Swal.getTimerLeft() / 1000)}`;
-          }, 100);
-        },
-        willClose: () => {
-          clearInterval(timerInterval);
-        },
-      });
-      return true;
-    }
-  }
-
-  //si el usuario se registra exitosamente:
-
-  //si el usuario no esta validado completamente:
-  function errorHandling(messageType, text) {
-    Swal.fire({
-      icon: messageType,
-      title: "Oops...",
-      text: text,
-    });
-  }
-});
-
-=======
 /* Mostrar opcion de administrador si el admin es el usuario Logeado*/
-const AdminLogged = true;
-
-window.onload = function () {
-	const AdminUser = document.getElementById('AdminUser');
-	if (AdminLogged) {
-		AdminUser.style.display = 'block';
-	} else {
-		AdminUser.style.display = 'none';
-	}
-};
-/* Mostrar opcion de administrador si el admin es el usuario Logeado*/
->>>>>>> 476924d952a5cf74e5f297c3aef4278e1336256e
