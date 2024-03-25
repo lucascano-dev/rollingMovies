6624ac6232513fedf8c04225caafff02503587df
Leonardo Balbastro <leonardonbalbastro@gmail.com>
2024-03-24 21:52:53 -0300

    finalizacion de modal detalle de pelicula

--- js/PaginaPrincipal.js
+++ js/PaginaPrincipal.js
@@ -1,11 +1,11 @@
-/*Menu Desplegable*/
+
 function toggleMenu() {
   var menu = document.getElementById("menu");
   menu.classList.toggle("active");
 }
-/*Menu Desplegable*/
 
-/* Mostrar opcion de administrador si el admin es el usuario Logeado*/
+
+
 const AdminUser = document.getElementById('AdminUser')
 
 if (localStorage.getItem('userLogged') !== undefined) {
@@ -22,23 +22,20 @@ if (localStorage.getItem('userLogged') !== undefined) {
 };
 
 
-/* Mostrar opcion de administrador si el admin es el usuario Logeado*/
 
-//Manejador de imagenes modal detalle de pelicula.
+
+
 const miImagen = document.querySelectorAll("img.imgButton")
-// console.log(miImagen)
 miImagen.forEach((imagen) => {
   imagen.addEventListener('click', () => {
+
     let title = imagen.alt
     let movies = JSON.parse(localStorage.getItem('movies'))
     let movieFound = movies.find((movie) => movie.nombre == title)
     let modalInfo = document.getElementById('modal-body')
     let modalContent = document.getElementById('bg-image')
-    let modalDialog = document.getElementById('modal-dialog')
-    //botton cerrar
-
-
     let buttonClose = document.createElement('button');
+
     buttonClose.type = "button"
     buttonClose.className = "btn-close mt-3 me-3"
     buttonClose.setAttribute('data-bs-dismiss', 'modal')
@@ -48,43 +45,63 @@ miImagen.forEach((imagen) => {
     buttonClose.style.right = 0;
     buttonClose.style.backgroundColor = "white"
     buttonClose.style.zIndex = "3"
-    let fullbuildbutton = buttonClose.outerHTML;
-    modalContent.innerHTML = fullbuildbutton
-    console.log(fullbuildbutton)
-    //fin boton cerrar
-    if (modalContent.style.backgroundImage) {
-      modalContent.style.removeProperty('background-image')
-    }
-    // console.log(movieFound)
+    buttonClose.id = "buttonClose"
+
+
+
     if (movieFound !== undefined) {
-      imagen.setAttribute('data-bs-toggle', 'modal')
-      imagen.setAttribute('data-bs-target', '#detallePelicula')
-      console.log(modalContent.style.backgroundImage)
-      // style="height: 80vh; width: 50vw; background-position: 50% center; background-repeat: no-repeat; background-size: cover;"
-      modalContent.style.height = "80vh"
-      modalContent.style.width = "50vw"
-      modalContent.style.backgroundImage = `url('./assets/img/${title}.png')`;
-      modalContent.style.backgroundPosition = "50% center"
-      modalContent.style.backgroundRepeat = "no-repeat"
-      modalContent.style.backgroundSize = "cover"
-
-      modalInfo.style.opacity = "1"
-      modalInfo.className = "container h-100"
-      // modalInfo.textContent = "Something"
-
+      modalInfo.className = "container-fluid d-flex align-items-center m-auto h-100 w-100 p-0"
+      let divBodyMovieDetail = document.createElement('div');
+      let divInnerModalMovieDetail = document.createElement('div')
+      let h1 = document.createElement('h1')
+      let spanDescription = document.createElement('p')
+      let videoLink = movieFound.urlVideo.substr(32)
+      let video = `<iframe id="miVideo" src="https://www.youtube.com/embed/${videoLink}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+      divBodyMovieDetail.className = "modal-title"
+      video.id = "video"
+      h1.textContent = `${title.toUpperCase()}`
+      h1.style.textAlign = "center"
+      h1.style.margin = "1rem";
+      h1.style.padding = "1rem"
+      spanDescription.textContent = `${movieFound.descripcion}`
+      spanDescription.id = "descripcion"
+      spanDescription.className = "d-none d-md-block"
+      divInnerModalMovieDetail.className = "container-fluid"
+      divBodyMovieDetail.style.color = "White"
+      divBodyMovieDetail.style.backgroundColor = "rgba(0,0,0,0.7)"
+      divBodyMovieDetail.className = "border rounded"
+      divBodyMovieDetail.id = "modal-detail"
+      divBodyMovieDetail.style.height = "100%"
+      divBodyMovieDetail.style.width = "100%"
+      divBodyMovieDetail.style.backgroundSize = "cover"
+      divBodyMovieDetail.style.backgroundImage = `url('./assets/img/${title}.png')`;
+      divBodyMovieDetail.style.backgroundRepeat = "no-repeat"
+      divInnerModalMovieDetail.innerHTML = buttonClose.outerHTML
+      divInnerModalMovieDetail.append(h1)
+      divInnerModalMovieDetail.append(spanDescription)
+      divInnerModalMovieDetail.innerHTML += video
+      divBodyMovieDetail.appendChild(divInnerModalMovieDetail)
+      modalInfo.innerHTML = divBodyMovieDetail.outerHTML
+      // modalContent.style.height = "80vh"
+      // modalContent.style.width = "80vw"
+      // modalContent.style.backgroundImage = `url('./assets/img/${title}.png')`;
+      // modalContent.style.backgroundPosition = "50% center"
+      // modalContent.style.backgroundRepeat = "no-repeat"
+      // modalContent.style.backgroundSize = "cover"
+      // modalContent.id = "modal-content"
       modalContent.appendChild(modalInfo)
 
-      //div para el body de modalInfo
-      let divBodyMovieDetail = document.createElement('div');
-      divBodyMovieDetail.className = "modal-title"
-      divBodyMovieDetail.textContent = `${title}`
-      // divBodyMovieDetail.style.backgroundColor = "FFF"
-      divBodyMovieDetail.style.color = "White"
-      // divBodyMovieDetail.style.opacity = 10
 
-      modalInfo.innerHTML = divBodyMovieDetail.outerHTML
     }
+    document.getElementById('detallePelicula').addEventListener('hidden.bs.modal', function () {
+      // Obtener el elemento de video
+      let videoElement = document.getElementById('miVideo');
+      // Eliminar el elemento de video del DOM
+      videoElement.parentNode.removeChild(videoElement);
+    });
   })
+  imagen.setAttribute('data-bs-toggle', 'modal')
+  imagen.setAttribute('data-bs-target', '#detallePelicula')
 })
 
 const adminHandler = () => {
